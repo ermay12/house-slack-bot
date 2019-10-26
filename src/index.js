392518e365dbec16daf567521155765877f463ca
Eric Maynard <ermay12@gmail.com>
2019-10-26 18:20:15 -0400

    added logging

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const config = require("../config.json");
 
 const express = require("express");
 const request = require("request");
+const fs = require("fs");
 const app = express();
 app.use(express.json());
 
@@ -10,6 +11,23 @@ app.listen(config.port, () =>
 );
 
 app.post("/", function(req, res) {
+  fs.appendFile(
+    `./logs/posts.txt`,
+    new Date(Date.now()).toString() +
+      "\n\n" +
+      JSON.stringify(req.headers) +
+      "\n\n" +
+      JSON.stringify(req.url) +
+      "\n\n" +
+      JSON.stringify(req.params) +
+      "\n\n" +
+      JSON.stringify(req.body) +
+      "\n-------------------------------------\n\n",
+    err => {
+      if (err) throw err;
+      console.log("Post saved!");
+    }
+  );
   try {
     res.send(req.body.challenge);
   } catch (error) {
