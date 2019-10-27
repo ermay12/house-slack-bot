2dfc459277c90ae075165f9c37966f3cfaefbaf5
Eric Maynard <ermay12@gmail.com>
2019-10-27 12:35:51 -0400

    added commands

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const config = require("../config.json");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const request = require("request");
 const fs = require("fs");
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(config.port, () =>
   console.log(`Server running on port ${config.port}`)
 );
 
-app.post("/", function(req, res) {
+function logRequest(req) {
+  console.log("new post");
   fs.appendFile(
     `./logs/posts.txt`,
     new Date(Date.now()).toString() +
@@ -28,8 +31,64 @@ app.post("/", function(req, res) {
       console.log("Post saved!");
     }
   );
+}
+
+function verifyRequest(req) {
+  return req.body.token === config.token;
+}
+
+function sendMessage(message) {
+  console.log("sending message");
+  request(
+    {
+      method: "POST",
+      uri: config.webhook,
+      body: { text: message },
+      json: true
+    },
+    function(error, response, body) {
+      if (error) {
+        return console.error("Failed to send message:", error);
+      }
+      console.log("message sent.");
+    }
+  );
+}
+
+function processCommand(req, res) {
+  sendMessage("hi did someone call me?");
+  res.send("success!");
+}
+
+function processNewMessage(req) {}
+
+app.post("/", function(req, res) {
+  logRequest(req);
   try {
-    res.send(req.body.challenge);
+    if (!verifyRequest(req)) {
+      console.log("invalid request");
+      res.send("Failure: invalid token");
+      return;
+    }
+    if (res.body.type === "url_verification") {
+      res.send(req.body.challenge);
+      return;
+    }
+    processNewMessage(req, res);
+  } catch (error) {
+    res.send("Failure: " + error);
+  }
+});
+
+app.post("/command", function(req, res) {
+  logRequest(req);
+  try {
+    if (!verifyRequest(req)) {
+      console.log("invalid request");
+      res.send("Failure: invalid token");
+      return;
+    }
+    processCommand(req, res);
   } catch (error) {
     res.send("Failure: " + error);
   }
